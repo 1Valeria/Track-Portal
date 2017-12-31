dc45cff12831765e1f696279b6fba0ae60dc121c
unknown <shpakovskaya.valeriya.14@gmail.com>
2017-12-31 17:21:22 +0300

    This is a new commit

---
+++ src/main/java/com/trackportal/portalart/PortalartApplication.java
@@ -0,0 +1,12 @@
+package com.trackportal.portalart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalartApplication.class, args);
+	}
+}
